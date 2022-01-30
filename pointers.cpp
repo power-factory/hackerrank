3b72ef8af0b4f38447b155f364090ef0748a60b7
Adam Pietrzyk <adam@powerfactory.cz>
2022-01-30 22:45:34 +0100

    2022.01.30 Adam: Enhanced version of basic demo of pointers memory allocation. My personal playground.

--- pointers.cpp
+++ pointers.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <climits>
 
 int main()
 {
@@ -14,6 +15,8 @@ int main()
     std::cout << "Wielkość zajmowanych danych to:"<<std::endl;
     std::cout << "Wielkość zmiennej x:          " << sizeof(variable) <<std::endl;
     std::cout << "Wielkość wskaznika na x:      " << sizeof(pointerVariable) <<std::endl;
+
+    std::cout << "MAX dla int w GCC x86_64 Linux: " << INT_MAX<<std::endl;
 
     return 0;
 }
